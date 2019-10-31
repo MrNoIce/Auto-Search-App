3065cf4a61ccc86c8bda4eea4eb5183d47359eca
Jake Scott <bemybanker@gmail.com>
2019-10-30 20:05:27 -0500

    gitignore didnt work

--- autosearchapp/views/vehicles/search.py
+++ autosearchapp/views/vehicles/search.py
@@ -15,7 +15,7 @@ import requests
 def search(request):
     url = "http://api.marketcheck.com/v1/search"
 
-    querystring = {"api_key":"{API_KEY}","year":"2014","make":"ford","latitude":"34.05","longitude":"-118.24","radius":"100","car_type":"used","start":"0","rows":"50","seller_type":"dealer"}
+    querystring = {"api_key":{API_KEY},"year":"2014","make":"ford","latitude":"34.05","longitude":"-118.24","radius":"100","car_type":"used","start":"0","rows":"50","seller_type":"dealer"}
 
     headers = {
         'Host': "marketcheck-prod.apigee.net",
